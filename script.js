7846689eab07360e680f850ff7ca8712d86edd21
Júlio César Mesquita Camilo Martins <94556951+devjuliomesquita@users.noreply.github.com>
2022-04-06 01:48:18 -0300

    Versao_07
    Iniciado a função create

--- script.js
+++ script.js
@@ -6,9 +6,23 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active');
 
+const tempClient = {
+    nome:"Júlio César",
+    email:"juliocesarmcamilo@gmail.com",
+    celular: "32165432165",
+    cidade: "Caucaia"
+}
 //CHAMANDO AS FUNÇÕES
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
+
+//CRUD - create read update delete
+const createClient = (client) => {
+    const db_client = JSON.parse(localStorage.getItem('db_client'));
+    db_client.push(client);
+    localStorage.setItem("db_cliente", JSON.stringify(client));
+    
+}
