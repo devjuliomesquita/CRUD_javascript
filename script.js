93feab272c1879de63a55f1738bb528aba8d1787
Júlio César Mesquita Camilo Martins <94556951+devjuliomesquita@users.noreply.github.com>
2022-04-07 05:48:09 -0300

    Versao_10
    Função cadastrar criada porém deve ser melhorada...

--- script.js
+++ script.js
@@ -12,20 +12,14 @@ const tempClient = {
     celular: "32165432165",
     cidade: "Caucaia"
 }
-//CHAMANDO AS FUNÇÕES
-document.getElementById('cadastrarCliente')
-    .addEventListener('click', openModal)
-
-document.getElementById('modalClose')
-    .addEventListener('click', closeModal)
 
 const getLocalStorange = () => JSON.parse(localStorage.getItem("db_client")) ?? [];
-const setLocalStorange = () => localStorage.setItem("db_client", JSON.stringify(client));
+const setLocalStorange = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient));
 
 //CRUD - create read update delete
 //CREATE
 const createClient = (client) => {
-    const dbClient = getLocalStorange()
+    const dbClient = getLocalStorange();
     dbClient.push(client);
     setLocalStorange(dbClient);
 }
@@ -45,3 +39,30 @@ const deleteClient = (index) => {
     dbClient.splice(index,1);
     setLocalStorange(dbClient)
 }
+//MÉTODOS QUE VERIFICA SE OS CAMPOS SÃO VÁLIDOS
+const isValidFields = () => {
+    document.getElementById('form').reportValidity();
+}
+
+//INTERAÇÃO COM O LAYOUT
+const saveClient = () => {
+    // SE OS CAMPOS SÃO VÁLIDOS
+    if(isValidFields()) {
+        const client = {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            phone: document.getElementById('phone').value,
+            city: document.getElementById('city').value
+        }
+        createClient(client);
+    }
+}
+
+//CHAMANDO AS FUNÇÕES
+document.getElementById('cadastrarCliente')
+    .addEventListener('click', openModal)
+
+document.getElementById('modalClose')
+    .addEventListener('click', closeModal)
+document.getElementById('btnSave')
+    .addEventListener('click', saveClient);
